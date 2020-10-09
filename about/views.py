a8354b4b784d6b3890c1782cd0d16674d1856563
rajeshpai1 <rajeshp0911@gmail.com>
2020-10-09 15:55:06 +0530

    first commit

---
+++ about/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+# Create your views here.
+def about_view(request):
+    template_name = "about.html"
+    return render(request, template_name)
